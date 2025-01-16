df3adacec3575390998a72c69f40e56f0c493a69
timniklas <accounts@tnkoch.de>
2025-01-16 22:02:15 +0100

    Create sensor.py

---
+++ custom_components/meshtastic/sensor.py
@@ -0,0 +1,31 @@
+import logging
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import UnitOfPower, UnitOfElectricPotential, UnitOfEnergy
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import MeshtasticCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    """Set up the Sensors."""
+    # This gets the data update coordinator from hass.data as specified in your __init__.py
+    coordinator: MeshtasticCoordinator = hass.data[DOMAIN][
+        config_entry.entry_id
+    ].coordinator
+
+    _LOGGER.warn(coordinator.data.nodes)
