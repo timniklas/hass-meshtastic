f202f1674f7e12c72d3a3c0dbf21f60a06673041
timniklas <accounts@tnkoch.de>
2025-01-17 10:25:10 +0100

    Update config_flow.py

--- custom_components/meshtastic/config_flow.py
+++ custom_components/meshtastic/config_flow.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
 from typing import Any
-
 import voluptuous as vol
+from functools import partial
 
-from homeassistant.components.bluetooth import (
-    BluetoothServiceInfoBleak,
-    async_discovered_service_info,
-)
+import meshtastic
+import meshtastic.tcp_interface
+
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_ADDRESS, CONF_TYPE
 from homeassistant.data_entry_flow import FlowResult
@@ -15,33 +14,34 @@ from homeassistant.helpers.selector import selector
 
 from .const import DOMAIN
 
-
 class MeshtasticConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(
-        self,
-        user_input: dict[str, Any] | None = None,
-    ) -> FlowResult:
+    async def async_step_user(self, formdata):
         return self.async_show_menu(
-          step_id="user", menu_options=[
-            "tcp"
-          ])
+            step_id="user", menu_options=[
+                "tcp"
+            ])
 
-    async def async_step_tcp(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        if user_input is not None:
-            device_address = user_input[CONF_ADDRESS]
+    async def async_step_tcp(self, formdata):
+        if formdata is not None:
+            device_address = formdata[CONF_ADDRESS]
+            #try to connect
+            f_kwargs = {'hostname': device_address}
+            await self.hass.async_add_executor_job(
+                partial(meshtastic.tcp_interface.TCPInterface, **f_kwargs)
+            )
+            #check if unique
             await self.async_set_unique_id(device_address)
             self._abort_if_unique_id_configured()
+            #create entity
             return self.async_create_entry(title=f"Meshtastic {device_address}", data={
-                CONF_ADDRESS: address,
+                CONF_ADDRESS: device_address,
                 CONF_TYPE: 'tcp'
             })
     
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({
-              vol.Required(CONF_ADDRESS): str
+            step_id="tcp", data_schema=vol.Schema({
+                vol.Required(CONF_ADDRESS): str
             })
         )
